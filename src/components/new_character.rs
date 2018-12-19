5eab12d79638bfbf6442694b1c413e60aaf23032
lenscas <lenscas@gmail.com>
2018-12-20 00:05:43 +0100

    Add semi functional modal

--- src/components/new_character.rs
+++ src/components/new_character.rs
@@ -1,15 +1,17 @@
+use crate::components::health_bar::HealthBarProps;
+use crate::components::health_bar::health_bar;
 use crate::components::character::Character;
 use crate::components::character::CharacterTypes;
+
 use yew::prelude::*;
 
 pub struct NewCharacter {
 	money_left: i64,
 	on_buy : Option<Callback<(Character)>>,
-	is_human : bool,
 }
 
 pub enum Msg {
-	BuyChar,
+	BuyChar(Character),
 }
 #[derive(PartialEq, Clone)]
 pub struct Props {
@@ -32,20 +34,14 @@ impl<CTX: 'static> Component<CTX> for NewCharacter {
 		NewCharacter {
 			on_buy: props.on_buy,
 			money_left : 0,
-			is_human: false,
 		}
 	}
 	fn update(&mut self, msg: Self::Message, _: &mut Env<CTX, Self>) -> ShouldRender {
 		match msg {
-			Msg::BuyChar => {
+			Msg::BuyChar(character) => {
 				self.money_left = self.money_left + 1;
 				if let Some(ref mut callback) = self.on_buy {
-					if self.is_human {
-						callback.emit(Character::create_character(CharacterTypes::Human));
-					} else {
-						callback.emit(Character::create_character(CharacterTypes::Merfolk));
-					}
-					self.is_human = !self.is_human;
+					callback.emit(character.clone());
 				}
 				true
 			},
@@ -68,7 +64,6 @@ impl<CTX: 'static> Renderable<CTX, NewCharacter> for NewCharacter {
 				</div>
 				<div class="col",>
 					<button
-						onclick=|_|Msg::BuyChar,
 						class="btn",
 						class="btn-success",
 						data-toggle="modal",
@@ -89,22 +84,60 @@ impl NewCharacter {
 				<div class="modal-dialog", role="document",>
 					<div class="modal-content",>
 						<div class="modal-header",>
-							<h5 class="modal-title",>{"Modal title"}</h5>
+							<h5 class="modal-title",>{"Buy character"}</h5>
 							<button type="button", class="close", data-dismiss="modal", aria-label="Close",>
-								<span aria-hidden="true",>{"&times;"}</span>
+								<span aria-hidden="true",>{"X"}</span>
 							</button>
 						</div>
 						<div class="modal-body",>
-							<p>{"Modal body text goes here."}</p>
-						</div>
-						<div class="modal-footer",>
-							<button type="button", class=("btn","btn-primary"),>{"Save changes"}</button>
-							<button type="button", class=("btn","btn-secondary"), data-dismiss="modal",>{"Close"}</button>
+							{self.render_character_selection(CharacterTypes::Human)}
+							{self.render_character_selection(CharacterTypes::Merfolk)}
+							{self.render_character_selection(CharacterTypes::Human)}
 						</div>
 					</div>
 				</div>
 			</div>
 		}
-	
+	}
+
+	fn render_character_selection<CTX: 'static>(&self, char_type : CharacterTypes) -> Html<CTX,Self> {
+		let character = Character::create_character(char_type);
+		let image = character.get_image();
+		let name = character.name.clone();
+		let max_health = character.max_health;
+		let cur_health = character.cur_health;
+		html! {
+			<li class="list-group-item", onclick=|_|Msg::BuyChar(character.to_owned()),>
+				<div class="row",>
+					<div class="col-md-3",>
+						<img class="img-fluid",alt={image.1}, src={image.0},/>
+					</div>
+					<div class="col",>
+						<h5>{name.clone()}</h5>
+						<div class="row",>
+							<div class="col-md-9",>
+								{
+									health_bar(
+										HealthBarProps {
+											max: max_health,
+											current: cur_health,
+											break_yellow:50,
+											break_red:20,
+										}
+									)
+								}
+							</div>
+							<div class="col",>
+								{"HP"}
+							</div>
+						</div>
+						<div class="row",>
+							<p>{"This is a nice description of this race"}</p>
+						</div>
+					</div>
+				</div>
+
+			</li>
+		}
 	}
 }
