4f6983b433625c8bca8222f3ffd8468c0af13f93
lenscas <lenscas@gmail.com>
2019-01-27 13:38:52 +0100

    Add species generation

--- src/classes/character.rs
+++ src/classes/character.rs
@@ -1,58 +1,37 @@
-use core::fmt;
-use stdweb::Value;
-use stdweb::unstable::TryInto;
-
-#[derive(PartialEq,Copy,Clone,Debug,Serialize,Deserialize)]
-pub enum CharacterTypes {
-	Human,
-	Merfolk
-}
-impl fmt::Display for CharacterTypes {
-	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		fmt::Debug::fmt(self, f)
-	}
-}
+use crate::generated::create_description::generate_description;
+use crate::generated::create_name::generate_name;
+use crate::generated::species_types::SpeciesTypes;
+use crate::generated::create_type::generate_type;
+use crate::generated::genereate_image::generate_image;
 
 #[derive(PartialEq,Clone,Serialize, Deserialize,Debug)]
 pub struct Character {
-	pub char_type : CharacterTypes,
+	pub char_type : SpeciesTypes,
 	pub name : String,
 	pub max_health: i64,
-	pub cur_health : i64
+	pub cur_health : i64,
+	pub image:String,
+	pub description : String
 }
 impl Character {
-	fn create_name(char_type : CharacterTypes) -> String {
-		let as_str = char_type.to_string();
-		let v: Value = js! {
-			return geneateName(@{as_str})
-		};
-		let v : String = v.try_into().expect("Something wend wrong");
-		v
-	}
-	pub fn create_character(char_type : CharacterTypes ) -> Character {
-		let name = Character::create_name(char_type);
-		match char_type {
-			CharacterTypes::Human => {
-				Character {
-					char_type : char_type,
-					name : name,
-					max_health: 1,
-					cur_health : 1
-				}
-			}
-			CharacterTypes::Merfolk => {
-				Character {
-					char_type : char_type,
-					name : name,
-					max_health: 5,
-					cur_health : 2
-				}
-			}
+	pub fn create_character() -> Character {
+		let species = generate_type();
+		let name = generate_name(&species);
+		let image = generate_image(&species);
+		let description = generate_description(&species);
+		Character {
+			char_type : species,
+			name : name,
+			max_health : 100,
+			cur_health :100,
+			image,
+			description
 		}
 	}
 	pub fn get_image(&self) -> (String,String){
 		(
-			String::from("/assets/images/")+&self.char_type.to_string() +".png",
+
+			self.image.clone(),
 			self.char_type.to_string()
 		)
 	}
